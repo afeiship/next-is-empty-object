bcc386a34e75fe5b14faa8038bad37d0dbba2d48
afeiship <1290657123@qq.com>
2019-02-16 12:41:25 +0800

    feat: more nature empty-object

--- src/next-is-empty-object.js
+++ src/next-is-empty-object.js
@@ -1,11 +1,16 @@
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
+  var toString = Object.prototype.toString;
+  var NATURE_OBJECT = '[object Object]';
+  // object.keys() only return enumerable props
 
   nx.isEmptyObject = function(inObj) {
-    var key;
-    for (key in inObj) return false;
-    return true;
+    if (!inObj) return false;
+    if (toString.call(inObj) === NATURE_OBJECT) {
+      return !Object.keys(inObj).length;
+    }
+    return false;
   };
 
   if (typeof module !== 'undefined' && module.exports) {
--- test/test.js
+++ test/test.js
@@ -1,15 +1,17 @@
 var nx = require('next-js-core2');
 require('../src/next-is-empty-object');
 
-
-test('nx.isEmptyObject', function () {
-  var obj1 = {name: 'fei'};
-  var obj2 = {email: '1290657123@qq.com'};
-
-  var result = {};
-
-  nx.isEmptyObject(result, obj1, obj2);
-
-  expect(result.name, obj1.name).toBe(null);
+test('nx.isEmptyObject', function() {
+  expect(nx.isEmptyObject(function(){})).toBe(false);
+  expect(nx.isEmptyObject(null)).toBe(false);
+  expect(nx.isEmptyObject(undefined)).toBe(false);
+  expect(nx.isEmptyObject(new Date())).toBe(false);
+  expect(nx.isEmptyObject(true)).toBe(false);
+  expect(nx.isEmptyObject(false)).toBe(false);
+  expect(nx.isEmptyObject(0)).toBe(false);
+  expect(nx.isEmptyObject('s')).toBe(false);
+  expect(nx.isEmptyObject('')).toBe(false);
+  expect(nx.isEmptyObject({})).toBe(true);
+  expect(nx.isEmptyObject(Object.create(null))).toBe(true);
+  expect(nx.isEmptyObject({ a: 1 })).toBe(false);
 });
-
